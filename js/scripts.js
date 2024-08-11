b4e5f18019385d4494b67d4aa2589260d945b09d
andreazevedo80 <andre.l.azevedo69@gmail.com>
2024-08-11 12:50:27 -0300

    Adicionando novas seções

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
-const palavras = ["o seu negócio", "seus projetos", "seu futuro digital", "sua infraestrutura"];
-let index = 0;
+const palavras = ["o seu negócio", "a sua inovação", "o seu futuro"];
+let indiceAtual = 0;
 
-function alternarPalavra() {
-    document.getElementById("palavra-alternada").innerText = palavras[index];
-    index = (index + 1) % palavras.length;
+function alternarPalavras() {
+    const elemento = document.getElementById("palavra-alternada");
+    elemento.textContent = palavras[indiceAtual];
+    indiceAtual = (indiceAtual + 1) % palavras.length;
 }
 
-setInterval(alternarPalavra, 2000); // Alterna a cada 2 segundos
+setInterval(alternarPalavras, 2000);
+
+
+    function irParaPagina() {
+        window.location.href = './pages/solucoes.html' // Redireciona para a página solucoes.html
+    }
+
+    // Função para adicionar um efeito de animação ao botão
+    document.querySelector('.btn-saiba-mais').addEventListener('mouseover', function() {
+        this.style.backgroundColor = '#FF8C00' // Altera a cor de fundo ao passar o mouse
+    });
+
+    document.querySelector('.btn-saiba-mais').addEventListener('mouseout', function() {
+        this.style.backgroundColor = 'var(--cor-secundaria)' // Restaura a cor de fundo original
+    });
